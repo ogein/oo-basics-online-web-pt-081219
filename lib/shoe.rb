a55a420bdc240863a95bd9bc89ce012b4ed27690
Ogein Imoukhuede <o.imoukhuede@aol.com>
2019-09-21 01:17:10 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+
+class Shoe 
+  
+  attr_accessor :brand :color :size :material :condition
+  
+  def intialize(brand)
+    @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+  
+  def cobble
+    puts ""
